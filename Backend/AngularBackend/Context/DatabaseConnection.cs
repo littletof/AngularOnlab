ce3555446cc0f6809bee3fd65283b5a1cec2e154
Szalay Kristóf <szalay.kristof@upcmail.hu>
2018-02-08 20:30:52 +0100

    Backend init

---
+++ Backend/AngularBackend/Context/DatabaseConnection.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using AngularBackend.Models;
+
+namespace AngularBackend.Context {
+    public class DatabaseContext : DbContext {
+        public DatabaseContext() : base("DefaultConnection") { }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ Backend/AngularBackend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularBackend.WebApiApplication" Language="C#" %>
