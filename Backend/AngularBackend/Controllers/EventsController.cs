5a987afaad4e4098c0c482eaf79fa51e5b5492e0
Szalay Kristóf <szalay.kristof@upcmail.hu>
2018-02-10 22:19:14 +0100

    proper backend get

--- Backend/AngularBackend/Controllers/EventsController.cs
+++ Backend/AngularBackend/Controllers/EventsController.cs
@@ -14,19 +14,63 @@ namespace AngularBackend.Controllers {
         private DatabaseContext db = new DatabaseContext();
 
         // GET api/<controller>
-        public IEnumerable<Event> Get() {
-            return new Event[] { new Event(), new Event() };
+        public IHttpActionResult Get() {
+            try {
+                //TODO move to data class
+                var result = from even in db.Events
+                             select new {
+                                 even.EventId,
+                                 even.Name,
+                                 even.Description,
+                                 Entry = from entry in db.Entries
+                                         where entry.EventId == even.EventId
+                                         select new {
+                                             entry.Name,
+                                             entry.Note,
+                                             entry.EventId
+                                         }
+
+                             };
+
+
+                return Ok(result);
+            } catch (Exception) {
+                //If any exception occurs Internal Server Error i.e. Status Code 500 will be returned  
+                return InternalServerError();
+            }
         }
 
         // GET api/<controller>/5
-        public Event Get(int id) {
-            var e = new Event();
-            e.EventId = id;
-            return e;
+        public IHttpActionResult Get(int id) {
+            try {
+                //TODO move to data class
+                var result = from even in db.Events
+                             where even.EventId == id
+                             select new {
+                                 even.EventId,
+                                 even.Name,
+                                 even.Description,
+                                 Entry = from entry in db.Entries
+                                         where entry.EventId == even.EventId
+                                         select new {
+                                             entry.Name,
+                                             entry.Note,
+                                             entry.EventId
+                                         }
+
+                             };
+
+
+                return Ok(result);
+            } catch (Exception) {
+                //If any exception occurs Internal Server Error i.e. Status Code 500 will be returned  
+                return InternalServerError();
+            }
         }
 
         // POST api/<controller>
         public void Post([FromBody]Event value) {
+            Console.WriteLine(value);
         }
 
         // PUT api/<controller>/5
