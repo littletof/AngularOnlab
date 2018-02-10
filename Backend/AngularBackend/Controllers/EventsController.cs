f868504abebbeedc8e7a3e5d0170b97ca209d8fa
Szalay Kristóf <szalay.kristof@upcmail.hu>
2018-02-10 16:30:00 +0100

    Events controller

---
+++ Backend/AngularBackend/Controllers/EventsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using AngularBackend.Models;
+using AngularBackend.Context;
+
+namespace AngularBackend.Controllers {
+    public class EventsController : ApiController {
+
+        private DatabaseContext db = new DatabaseContext();
+
+        // GET api/<controller>
+        public IEnumerable<Event> Get() {
+            return new Event[] { new Event(), new Event() };
+        }
+
+        // GET api/<controller>/5
+        public Event Get(int id) {
+            var e = new Event();
+            e.EventId = id;
+            return e;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]Event value) {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]Event value) {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id) {
+        }
+    }
+}
