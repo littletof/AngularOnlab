fe1c09eb135695e3d7f7410cc3c6aabd74886975
Szalay Kristóf <szalay.kristof@upcmail.hu>
2018-02-15 20:53:12 +0100

    init moment

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment/moment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor() { moment.locale('hu'); }
 }
