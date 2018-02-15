2cc4d7dee34e5b2eb7d7712f5e47eff23cfa3f50
Szalay Kristóf <szalay.kristof@upcmail.hu>
2018-02-15 22:53:34 +0100

    implemented selected date

--- AngularApp/src/app/view/new-event/new-event.component.ts
+++ AngularApp/src/app/view/new-event/new-event.component.ts
@@ -1,36 +1,68 @@
-import { Component, OnInit, ViewChild, ContentChild, AfterContentInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ContentChild, AfterContentInit, DoCheck, IterableDiffers } from '@angular/core';
 
 import * as moment from 'moment';
 import { MultipleDatePickerComponent, DateRangeHelper } from 'multiple-date-picker-angular/dist';
-import { PickerHacker } from '../hack-multiple-date-picker';
+import { Moment } from 'moment';
 
 @Component({
   selector: 'app-new-event',
   templateUrl: './new-event.component.html',
   styleUrls: ['./new-event.component.css']
 })
-export class NewEventComponent implements OnInit {
+export class NewEventComponent implements OnInit, DoCheck {
 
-  outp: Array<moment.Moment> = new Array();
+  selectedDays: Array<Moment> = new Array();
+  errorDays: Array<Moment> = new Array();
 
-  allowed: Array<moment.Moment> = new Array(moment());
+  allowed: Array<Moment> = new Array(moment());
   disabledDays: Array<any> = [];
 
   @ViewChild(MultipleDatePickerComponent)
   datePicker: MultipleDatePickerComponent;
 
-  constructor() {}
-
-  ngOnInit() {
-    PickerHacker.hackDatePicker(this.datePicker, this.allowed);
+  differ: any;
+  constructor(differs: IterableDiffers) {
+     this.differ = differs.find([]).create(null);
   }
 
+  ngDoCheck() {
+    const change = this.differ.diff(this.selectedDays);
+    if (change) {
+      this.sort();
+    }
+  }
+
+  ngOnInit() {
+    // PickerHacker.hackDatePicker(this.datePicker, this.allowed);
+  }
+
+  sort() {
+    this.selectedDays.sort((a, b) => (a.unix() - b.unix()));
+  }
+
+  selectChange(daysOff: number[]) {
+    const removed: Array<Moment> = this.selectedDays.filter(day => daysOff.indexOf(day.isoWeekday() % 7) !== -1 );
+    this.errorDays = removed;
+  }
+
+  error(day: Moment): boolean {
+    return this.errorDays.indexOf(day) !== -1;
+  }
+
+  delDay(day: Moment) {
+    const index = this.selectedDays.indexOf(day);
+    if (index !== -1) {
+      this.selectedDays.splice(index, 1);
+    }
+    this.datePicker.runGenerate();
+  }
+  /*
   press() {
     this.allowed.push(moment('2018-02-16'));
     this.allowed.push(moment('2018-02-17'));
     this.allowed.push(moment('2018-02-18'));
     this.allowed.push(moment('2018-02-19'));
     (this.datePicker as any).refreshAllowed(this.allowed);
-  }
+  }*/
 
 }
