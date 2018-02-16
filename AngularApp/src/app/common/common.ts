414343d5f0f4b1719ae9c3ebc714f189b3ddd708
Szalay Kristóf <szalay.kristof@upcmail.hu>
2018-02-16 19:02:37 +0100

    common bővítés

--- AngularApp/src/app/common/common.ts
+++ AngularApp/src/app/common/common.ts
@@ -5,4 +5,22 @@ export class Common {
             array.splice(index, 1);
         }
     }
+
+    static contains(array: any[], item: any): boolean {
+        if (array) {
+           return array.indexOf(item) !== -1;
+        }
+        return false;
+    }
+
+    static mergeArray(a1: any[], a2: any[]): any[] {
+        const base: any[] = a1.slice();
+        a2.forEach(item => {
+            if (!this.contains(base, item)) {
+                base.push(item);
+                base.sort();
+            }
+        });
+        return base;
+    }
 }
